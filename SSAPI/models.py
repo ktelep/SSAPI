c0fc4350d5dbb239bcd946cfa74849b183d892fb
Kurt Telep <Kurt.Telep@emc.com>
2018-11-28 22:30:32 -0500

    Added Scrimmage Models and basic functionality

--- SSAPI/models.py
+++ SSAPI/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     lastname = db.Column(db.Text)
     roles = db.Column(db.Text)
     is_active = db.Column(db.Boolean, default=True, server_default='true')
+    invitations = db.relationship("ScrimmageInvite", backref='advisor')
 
     @property
     def rolenames(self):
@@ -36,3 +37,54 @@ class User(db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self,c.name) for c in self.__table__.columns if c.name not in "password"}
+
+class ScrimmageInvite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    accepted = db.Column(db.Boolean)
+    last_sent = db.Column(db.DateTime)
+    responded = db.Column(db.DateTime)
+    advisor_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    scrimmage_id = db.Column(db.Integer, db.ForeignKey("scrimmage.id"))
+
+    def as_dict(self):
+        return_dict = {c.name: getattr(self,c.name) for c in self.__table__.columns}
+        # Note we don't need to follow the relationships here, as the foreign
+        # keys will provide with the ID of the related object
+
+        return return_dict
+
+# Many-to-Many reference tables for Scrimmage class
+presenters = db.Table('presenters',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('scrimmage_id', db.Integer, db.ForeignKey('scrimmage.id'), primary_key=True)
+    )
+
+advisors = db.Table('advisors',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('scrimmage_id', db.Integer, db.ForeignKey('scrimmage.id'), primary_key=True)
+    )
+
+class Scrimmage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.Text)
+    schedule = db.Column(db.Text)
+    scrimmage_type = db.Column(db.Text)
+    scrimmage_complete = db.Column(db.Boolean) 
+    max_advisors = db.Column(db.Integer)
+    presenters = db.relationship('User', secondary = presenters, backref=db.backref('scrimmages_presenter', lazy=True))
+    advisors = db.relationship('User', secondary = advisors, backref=db.backref('scrimmages_advisor', lazy=True))
+    invites = db.relationship('ScrimmageInvite', backref="scrimmage")
+
+    def as_dict(self):
+        return_dict = {c.name: getattr(self,c.name) for c in self.__table__.columns}
+        # Follow the M2M relationships to return presenter and advisor IDs
+        return_dict["presenters"] = []
+        return_dict["advisors"] = []
+        for p in self.presenters:
+            return_dict["presenters"].append(p.id)
+        for a in self.advisors:
+            return_dict["advisors"].append(a.id)
+
+        return return_dict
+
+
--- setup_db.py
+++ setup_db.py
@@ -1,5 +1,5 @@
 from SSAPI import app, db, guard
-from SSAPI.models import User
+from SSAPI.models import User, Scrimmage, ScrimmageInvite
 
 guard.init_app(app,User)
 
@@ -13,28 +13,59 @@ db.session.add(User(
     roles='admin,presenter,advisor',
 ))
 
-db.session.add(User(
+presenter = User(
     username='presenter',
     password=guard.encrypt_password('presenter'),
     firstname='presenter',
     lastname='presenter',
     roles='presenter',
-))
+)
 
-db.session.add(User(
+advisor = User(
     username='advisor',
     password=guard.encrypt_password('advisor'),
     firstname='advisor',
     lastname='advisor',
     roles='advisor',
-))
+)
 
-db.session.add(User(
+present_advisor = User(
     username='presentadvisor',
     password=guard.encrypt_password('presentadvisor'),
     firstname='presentadvisor',
     lastname='presentadvisor',
     roles='presenter,advisor',
-))
+)
 
+db.session.add(presenter)
+db.session.add(advisor)
+db.session.add(present_advisor)
+
+new_scrimmage = Scrimmage(
+   subject="Test Scrimmage",
+   schedule="Test Schedule",
+   max_advisors = 20,
+   scrimmage_type = "Demo",
+   scrimmage_complete = False)
+
+new_scrimmage.presenters.append(presenter)
+new_scrimmage.advisors.append(advisor)
+new_scrimmage.advisors.append(present_advisor)
+
+new_scrimmage2 = Scrimmage(
+   subject="Test Scrimmage2",
+   schedule="Test Schedule2",
+   max_advisors = 20,
+   scrimmage_type = "Demo",
+   scrimmage_complete = False)
+
+new_scrimmage2.presenters.append(advisor)
+new_scrimmage2.advisors.append(presenter)
+
+new_invite = ScrimmageInvite(
+   scrimmage = new_scrimmage,
+   advisor = advisor
+   )
+db.session.add(new_scrimmage)
+db.session.add(new_scrimmage2)
 db.session.commit()
